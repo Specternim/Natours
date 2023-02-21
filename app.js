3d693ca2de8e82b33751e0812b98f97ac547f8b9
Vishal Alkari <vishalalkari999@gmail.com>
2023-02-21 17:16:56 +0530

    define mongodb hooks to modify the data before being sent to database.

--- app.js
+++ app.js
@@ -7,12 +7,11 @@ const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 
 // Built-in and 3rd party Middlewares
-
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
-app.use(express.json());
-app.use(express.static(`${__dirname}/public`));
+app.use(express.json()); // body parser
+app.use(express.static(`${__dirname}/public`)); // to use static files inside public folder
 
 // Custom Middleware
 app.use((req, res, next) => {
--- models/tourModel.js
+++ models/tourModel.js
@@ -1,58 +1,92 @@
 const mongoose = require('mongoose');
+const slugify = require('slugify');
 
-const tourSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, `A tour must have a name.`],
-    unique: true,
-    trim: true,
+const tourSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, `A tour must have a name.`],
+      unique: true,
+      trim: true,
+    },
+    slug: String,
+    difficulty: {
+      type: String,
+      required: [true, `A tour must have a difficulty level.`],
+    },
+    duration: {
+      type: Number,
+      required: [true, `A tour must have a duration.`],
+    },
+    price: {
+      type: Number,
+      required: [true, `A tour must have a price.`],
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now(),
+      select: false,
+    },
+    description: {
+      type: String,
+      trim: true,
+    },
+    imageCover: {
+      type: String,
+      required: [true, `A tour must have a cover image.`],
+    },
+    images: [String],
+    maxGroupSize: {
+      type: Number,
+      required: [true, `A tour must have a group size.`],
+    },
+    priceDiscount: Number,
+    ratingsQuantity: {
+      type: Number,
+      default: 0,
+    },
+    ratingsAverage: {
+      type: Number,
+      default: 4.5,
+    },
+    summary: {
+      type: String,
+      trim: true, // will remove white space in the content.
+      required: [true, `A tour must have a description.`],
+    },
+    startDates: [Date],
+    secretTour: {
+      type: Boolean,
+      default: false,
+    },
   },
-  difficulty: {
-    type: String,
-    required: [true, `A tour must have a difficulty level.`],
-  },
-  duration: {
-    type: Number,
-    required: [true, `A tour must have a duration.`],
-  },
-  price: {
-    type: Number,
-    required: [true, `A tour must have a price.`],
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now(),
-    select: false,
-  },
-  description: {
-    type: String,
-    trim: true,
-  },
-  imageCover: {
-    type: String,
-    required: [true, `A tour must have a cover image.`],
-  },
-  images: [String],
-  maxGroupSize: {
-    type: Number,
-    required: [true, `A tour must have a group size.`],
-  },
+  {
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true },
+  }
+);
 
-  priceDiscount: Number,
-  ratingsQuantity: {
-    type: Number,
-    default: 0,
-  },
-  ratingsAverage: {
-    type: Number,
-    default: 4.5,
-  },
-  summary: {
-    type: String,
-    trim: true, // will remove white space in the content.
-    required: [true, `A tour must have a description.`],
-  },
-  startDates: [Date],
+tourSchema.virtual('durationWeeks').get(function () {
+  return this.duration / 7;
+});
+
+// DOCUMENT MIDDLEWARE [MongoDB]: runs before save() & create()
+tourSchema.pre('save', function (next) {
+  this.slug = slugify(this.name, { lower: true });
+  next();
+});
+
+// QUERY MIDDLEWARE: can be used to define some private tours.
+tourSchema.pre(/^find/, function (next) {
+  this.find({ secretTour: { $ne: true } }); // exclude showing the tour that has secretTour set to true for all queries starting with find.
+  this.start = Date.now();
+  next();
+});
+
+// AGGREGATION MIDDLEWARE
+tourSchema.pre('aggregate', function (next) {
+  this.pipeline().unshift({ $match: { secretTour: { $ne: true } } });
+  next();
 });
 
 const Tour = mongoose.model('Tour', tourSchema);
