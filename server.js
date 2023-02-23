0dc1109e01f1aec171f2817424b381e78afd3080
Vishal Alkari <vishalalkari999@gmail.com>
2023-02-23 15:35:03 +0530

    update error handling

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
+process.on('uncaughtException', (err) => {
+  console.log(err.name, err.message);
+  console.log(`UNCAUGHT EXCEPTION! Shutting down...`);
+  process.exit(1);
+});
+
 dotenv.config({ path: './config.env' });
 const app = require('./app');
 
@@ -24,14 +30,6 @@ const server = app.listen(PORT, () =>
 process.on('unhandledRejection', (err) => {
   console.log(err.name, err.message);
   console.log(`UNHANDLED REJECTION! Shutting down...`);
-  server.close(() => {
-    process.exit(1);
-  });
-});
-
-process.on('uncaughtException', (err) => {
-  console.log(err.name, err.message);
-  console.log(`UNCAUGHT EXCEPTION! Shutting down...`);
   server.close(() => {
     process.exit(1);
   });
